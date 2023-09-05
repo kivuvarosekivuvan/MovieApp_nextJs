06ac6cd57358514e4ffdb0d4e04993afff02e41a
kivuvarosekivuvan <rosekivuvan@gmail..com>
2023-09-05 09:35:35 +0300

    Changed the styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { useEffect, useState } from "react";
+import "tailwindcss/tailwind.css";
 import {NEXT_PUBLIC_IMAGE_BASE_URL } from "./config";
 import { getMovies } from "./Utilities/utils";
-import { it } from "node:test";
+
 interface Movie {
   id: number;
   poster_path: string;
@@ -15,21 +16,23 @@ type MovieData = {
 };
 export default function Home() {
   const [movies, setMovies] = useState<MovieData | null>(null);
-  // const [isLoading , setIsLoading] = useState(true)
   useEffect(() => {
     (async () => {
       try {
         const moviesData = await getMovies();
         setMovies(moviesData);
         console.log({ movies: moviesData });
-      }
+      } 
       catch (error) {
         console.error('Error fetching movies:', error);
       }
+      
     })();
   }, []);
   return (
+    
   <main>
+    <p className="text-3xl "> Available Movies</p>
   <div className="grid grid-cols-5 gap-4 bg-white">
     {movies &&
       movies.results.map((item) => (
@@ -42,10 +45,12 @@ export default function Home() {
             <img
               src={`${NEXT_PUBLIC_IMAGE_BASE_URL}${item.poster_path}`}
               alt={item.title}
-              className="w-full h-auto mb-2 transition-opacity group-hover:opacity-75"
+              className="w-full h-auto mb-3 transition-opacity group-hover:opacity-90"
             />
+                           {/* <p className="text-base">{item.overview}</p> */}
+
             <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-75 text-white p-4 opacity-0 group-hover:opacity-100 transition-opacity">
-              <p className="text-base">{item.overview}</p>
+              {/* <p className="text-base">{item.overview}</p> */}
             </div>
           </div>
         </div>
